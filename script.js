cde0313909f7870311b47fb675d9053e34a17e0b
priyoshihara <priscila.yoshihara@usp.br>
2021-01-03 19:06:46 +0100

    video interaction

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("hello");
+
+$('#playlist li').each(function(){
+$(this).click(function(){
+var curUrl =$(this).attr("src");
+$('#videoarea').attr("src",curUrl)
+
+});
+});
+/*
+$("button").click(function() {
+    var $target         = "obscure"+$(this).attr("rel");
+    var $content_path   = "assets/";
+    var $vid_obj        = vjs("div_video");
+
+    // hide the current loaded poster
+    $("img.vjs-poster").hide();
+
+    $vid_obj.ready(function() {
+      // hide the video UI
+      $("#div_video_html5_api").hide();
+      // and stop it from playing
+      $vid_obj.pause();
+      // assign the targeted videos to the source nodes
+      $vid_obj.src([
+        { type: "video/mp4", src: $content_path+$target+".mp4" },
+        { type: "video/webm", src: $content_path+$target+".webm" },
+        { type: "video/ogg", src: $content_path+$target+".ogv" }
+      ]);
+      // replace the poster source
+      //$("img.vjs-poster").attr("src",$content_path+$target+".jpg").show();
+      // reset the UI states
+      $(".vjs-big-play-button").show();
+      $("#div_video").removeClass("vjs-playing").addClass("vjs-paused");
+      // load the new sources
+      $vid_obj.load();
+      $("#div_video_html5_api").show();
+    });
+});
+
+$("input[rel='01']").click();
+*/
